6ff8eb6ae2b7fd653c2623f6286d8c540b4bdece
Sreejith <sreejithakamenon@gmail.com>
2022-04-23 11:45:20 -0400

    po

--- server.js
+++ server.js
@@ -34,4 +34,10 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3000);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
+//server.listen(process.env.PORT || 5000)
