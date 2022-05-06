6567715b35c7a38c12a642ac406b84ce40fcd42a
Girish <girishamarbudhrani@gmail.com>
2022-05-06 18:04:50 -0400

    working join meeting

--- app.js
+++ app.js
@@ -92,4 +92,7 @@ io.on("connection", (socket) => {
 //     console.log("Your routes will be running on http://localhost:3000");
 // });
 
-server.listen(process.env.PORT || 443);
+server.listen(process.env.PORT || 443, () => {
+    console.log("We've now got a server!");
+    console.log("Your routes will be running on http://localhost:443");
+});
