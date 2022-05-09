6b21cdf38167e71b09381bc7e027df95937a6eba
Harsh16699 <hmp16699@gmail.com>
2022-05-08 23:03:48 -0400

    validation

--- data/users.js
+++ data/users.js
@@ -25,7 +25,11 @@ module.exports = {
             }
             if (checkemail) {
                 throw [400, `Another Account exists with same Email Address`];
-            } else {
+            } 
+            if (dob.charAt(1)!=="9" && dob.charAt(1)!=="0"){ throw [400, `Enter valid Date`]; }
+            if (dob.charAt(0)!=="1" && dob.charAt(0)!=="2"){ throw [400, `Enter valid Date`]; }
+            if ((dob.charAt(0) ==="2"|| dob.charAt(1) ==="0") && (dob.charAt(2)!=="0" && dob.charAt(2)!=="1")){ throw [400, `Enter valid Date`]; }
+            else {
                 let haspass = await bcrypt.hash(password, saltRound);
                 const newUser = {
                     username: username,
@@ -201,3 +205,50 @@ const checkCreateUser = function checkCreateUser(user, pass) {
     if (/\s/.test(pass)) throw [400, `Please input only charaters in an Password`];
     if (pass.length < 6) throw [400, `Please enter a valid password(atleast 4 characters long)`];
 };
+
+function checkField(fieldName, fieldValue) {
+    if (!fieldValue) throw `Error: You must supply a ${fieldName}!`;
+    if (typeof fieldValue !== 'string') throw `Error: ${fieldName} must be a string!`;
+    fieldValue = fieldValue.trim();
+    if (fieldValue.length === 0)
+        throw `Error: ${fieldName} cannot be an empty string or string with just spaces`;
+    if (fieldValue.includes(' ')) {
+        throw `Error: ${fieldName} should not contain any spaces`;
+    }
+    fieldValue = fieldValue.toLowerCase();
+    return fieldValue;
+}
+
+function checkEmail(email) {
+    if (!email) throw `Error: You must supply an email}!`;
+    if (typeof email !== 'string') throw `Error: email must be a string!`;
+    email = email.trim();
+    if (email.length === 0)
+        throw `Error: email cannot be an empty string or string with just spaces`;
+    if (email.includes(' ')) {
+        throw `Error: email should not contain any spaces`;
+    }
+    if (!this.validateEmail(email)) {
+        throw `Error: Enter a valid email`
+    }
+    email = email.toLowerCase();
+    return email;
+}
+
+function checkPassword(password) {
+    if (!password) throw `Error: You must supply a password!`;
+    if (typeof password !== 'string') throw `Error: password must be a string!`;
+    password = password.trim();
+    if (password.length === 0)
+        throw `Error: Password cannot be an empty string or string with just spaces`;
+    if (password.length < 6)
+        throw `Error: Password should be atleat 6 characters long`;
+    if (password.includes(' ')) {
+        throw 'Error: Password should not contain any spaces';
+    }
+    return password;
+}
+function checkDob(dateofbirth){
+if(!dateofbirth) throw `Error: You must supply date of birth`;
+if(dateofbirth[6]<9) throw `Error: Enter valid date`;
+}
